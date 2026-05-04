b7a1febea9d7ec5da4dd1123135409f0889c09b8
Decloedt <elmehdimouzoune@Decloedts-Mac-mini.local>
2026-05-04 12:54:43 +0100

    fix: stripe ios payment sheet + plist fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import Stripe
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    StripeAPI.defaultPublishableKey = "pk_test_51TRs2WPtBHJOXslcfJ1yDKC5i9xUHMF92CTQqg9PSWhakPgq8458qQQnBpNBqrjJ12psyA8qCV1OALh5vjmQjRAL00M4qF4j1Q" // ⚠️ TON KEY
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 

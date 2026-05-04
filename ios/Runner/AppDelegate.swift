44575cc914349504cf304060feb3f07af64818a6
DecloedtDevMas <maski.aymen@decloedt.cloud>
2026-05-04 18:36:30 +0100

    feature : secure api mobile 6

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import Stripe
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -8,8 +7,6 @@ import Stripe
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    StripeAPI.defaultPublishableKey = "pk_test_51TRs2WPtBHJOXslcfJ1yDKC5i9xUHMF92CTQqg9PSWhakPgq8458qQQnBpNBqrjJ12psyA8qCV1OALh5vjmQjRAL00M4qF4j1Q" // ⚠️ TON KEY
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
